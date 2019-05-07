25fab49adfbf848aa994384fe307329fb5b99b27
Jonathan Velez <mrvelez13@gmail.com>
2019-05-06 22:42:51 -0500

    merge con master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,7 +47,7 @@ const appRoutes: Routes = [
     MapaComponent,
     UsuarioComponent,
     LoadingComponent,
-    CuentaComponent
+    CuentaComponent,
     ModalCargaMasivaComponent
   ],
   imports: [
