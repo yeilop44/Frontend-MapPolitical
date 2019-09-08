65dacbfd6ba856bca2ac440921de5b883db59af8
Jonathan Velez <jonathan.v.giraldo@gmail.com>
2019-09-08 09:55:24 -0500

    desarrollo búsqueda de clientes

--- src/app/components/shared/contacts-table/contacts-table.component.ts
+++ src/app/components/shared/contacts-table/contacts-table.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contacts-table',
   templateUrl: './contacts-table.component.html'
 })
 export class ContactsTableComponent implements OnInit {
+
+  @Input() pageOfItems: any[]; 
 
   affiliates: number;
   
